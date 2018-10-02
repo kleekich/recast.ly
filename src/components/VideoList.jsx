c399dd36d3d27fb83595d7d607095d8f0b8a2360
kleekich <kangsik.lee@trizic.com>
2018-10-01 17:14:00 -0700

    Correct error with import module video data

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,3 +1,5 @@
+import exampleVideoData from '../data/exampleVideoData';
+
 var VideoList = () => (
   <div className="video-list">
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
@@ -16,4 +18,4 @@ VideoList.propTypes = {
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
-export default VideoList;
+export {default as VideoList} from 'src/components/VideoList';
