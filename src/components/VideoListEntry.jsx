152d58b467cd3a727e5bc6385cd7b34f9a761b0b
kleekich <kangsik.lee@trizic.com>
2018-10-01 21:17:36 -0700

    Refactoring videolist

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,30 @@
-var VideoListEntry = (props) => (
-
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src={props.videoObject.snippet.thumbnails.default.url} alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">{props.videoObject.snippet.title}</div>
-      <div className="video-list-entry-detail">{props.videoObject.snippet.description}</div>
-    </div>
-  </div>
+class VideoListEntry extends React.component (
+  constructor(props) {
+    super(props);
+    this.state = {
+      done:false;
+    };
+  }
+  onVideoClick() {
+    
+  }
+  
+  render(){
+    return (
+      <div className="videoItem" onClick={this.onVideoClick.bind(this)}>
+        <div className="video-list-entry media">
+          <div className="media-left media-middle">
+            <img className="media-object" src={this.props.videoObject.snippet.thumbnails.default.url} alt="" />
+          </div>
+          <div className="media-body">
+            <div className="video-list-entry-title">{this.props.videoObject.snippet.title}</div>
+            <div className="video-list-entry-detail">{this.props.videoObject.snippet.description}</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
 );
 
 // PropTypes tell other developers what `props` a component expects
